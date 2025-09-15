43787651bf524fc62189c95073d0b66cdfbe9368
Tamsil Amani <tamsajama@gmail.com>
2025-09-15 16:16:55 +0530

    Fix: VITE variable

--- frontend/src/components/TripForm.jsx
+++ frontend/src/components/TripForm.jsx
@@ -19,8 +19,8 @@ function TripForm({ onPlan }) {
         };
 
         try {
-            const API_URL = process.env.REACT_APP_API_URL;
-            console.log("API URL:", process.env.REACT_APP_API_URL);
+            const API_URL = process.env.VITE_API_URL;
+            console.log("API URL:", process.env.VITE_API_URL);
             const response = await fetch(`${API_URL}/api/plan/`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
